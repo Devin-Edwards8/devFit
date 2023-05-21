d194465f198b880453eed6086d50ea2ecb9b83b4
DevinEdwards <98787106+siiiiw@users.noreply.github.com>
2023-05-21 11:41:25 -0400

    finalized global color theme

--- src/global_colors.js
+++ src/global_colors.js
@@ -1,13 +1,6 @@
 // color theme
 
 const colorTheme = {
-    background: 'FFFFFF',
-    offBackground: '#f2f4f6',
-    highContrast: '353535',
-    boldAccent: '#284B63',
-    mediumAccent: '#94A5B1',
-    lightAccent: '#F2F4F6',
-
     background: '#FFFFFF',
     bars: '#353535',
     boldTheme: '#284B63',
